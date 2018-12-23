bc8115738ddd14ff4f2a569f9a0ddcf88dfed1d7
lutece <lallaworld1st@gmail.com>
2018-12-24 02:34:12 +0900

    test auto-build;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,12 +98,6 @@ const IndexPage: React.SFC<IndexProps> = props => {
         <meta property="og:url" content={config.siteUrl} />
         <meta property="og:image" content={props.data.header.childImageSharp.fluid.src} />
         <meta property="article:publisher" content={config.facebook} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={config.title} />
-        <meta name="twitter:description" content={config.description} />
-        <meta name="twitter:url" content={config.siteUrl} />
-        <meta name="twitter:image" content={props.data.header.childImageSharp.fluid.src} />
-        <meta name="twitter:site" content={`@${config.twitter.split('https://twitter.com/')[0]}`} />
         <meta property="og:image:width" content={width} />
         <meta property="og:image:height" content={height} />
       </Helmet>
